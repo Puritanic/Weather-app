773bc020c7b644b2cdb4a5dd4e389919e0487fb9
Puritanic <darktasevski@gmail.com>
2017-09-16 13:25:19 +0200

    Removed inline styling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -26,16 +26,14 @@ gulp.task('styles', function(){
 		.pipe( sourcemaps.init())
 		.pipe(postcss([
 			require('postcss-partial-import')({prefix: '_', extension: '.css'}),
-			require('postcss-assets')({
-				basePath: 'app'
-				}),
-				require("postcss-url")(),
-					fontMagician(),	// https://github.com/jonathantneal/postcss-font-magician	 		
-						require("postcss-cssnext")(),
-							rucksack(), // http://simplaio.github.io/rucksack/docs/#
-								require('postcss-nesting'),
-									lost(), // lost must be after nesting, so that media queries can work with it http://lostgrid.org/lostgrid-example.html
-										require("postcss-reporter")(),
+				require('postcss-assets')({ basePath: `app`, loadPaths: ['assets/images']}), // assets url handling
+					require("postcss-url")(),
+						fontMagician(),	// https://github.com/jonathantneal/postcss-font-magician	 		
+							require("postcss-cssnext")(),
+								rucksack(), // http://simplaio.github.io/rucksack/docs/#
+									require('postcss-nesting'),
+										lost(), // lost must be after nesting, so that media queries can work with it http://lostgrid.org/lostgrid-example.html
+											require("postcss-reporter")(),
 			]))
 		.on('error', gutil.log, function(err){
 			this.emit('end');
